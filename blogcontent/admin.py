cb550c1f92c150671d51e7808b47844ef6cf1e57
yuanzhang <ddyz1993@163.com>
2016-10-05 18:04:14 +0800

    m

--- blogcontent/admin.py
+++ blogcontent/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import timeNode
+from .models import blog
 class timeNodeAdmin(admin.ModelAdmin):
 	fields = ['type','time','todoList','doneList']
 admin.site.register(timeNode,timeNodeAdmin)
+class blogAdmin(admin.ModelAdmin):
+	fields = ['classify','created_time','content','title','tips']
+admin.site.register(blog,blogAdmin)
 # Register your models here.
--- blogcontent/models.py
+++ blogcontent/models.py
@@ -1,6 +1,15 @@
 from __future__ import unicode_literals
 from django.db import models
 
+class blog(models.Model):
+	classchoice=()
+	classify=models.CharField(max_length=200,choices=classchoice,default="other")
+	created_time=models.DateField(auto_now=False,auto_now_add=False)
+	content=models.TextField()
+	id=models.AutoField(primary_key=True)
+	title=models.CharField(max_length=200)
+	tips=models.CharField(max_length=500)
+	reading_num=models.IntegerField()
 class timeNode(models.Model):
 	typechoice=(
 		('year','year'),
